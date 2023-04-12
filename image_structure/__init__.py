1554815a34a4d26814fac601b0c82621757f93c6
KnightZhang <knight.625@live.com>
2023-04-12 13:43:56 +0100

    :sparkles: v0.2: add necessary structure to wrap the 	object from image.

---
+++ image_structure/__init__.py
@@ -0,0 +1,7 @@
+from .bounding_box import BoxList
+
+from .boxlist_ops import remove_small_boxes, cat_boxlist, choose_confident_boxes, boxlist_nms, boxlist_ml_nms
+
+from .geo_ins import GeoList
+
+from .image_list import to_image_list
