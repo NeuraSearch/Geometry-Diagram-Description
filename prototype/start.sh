10099edb0f59dbd5e68f7201d9b40f6abe611c2a
KnightZhang625 <knight.625@live.com>
2023-04-18 21:53:29 +0000

    :sparkles: v0.9: change bash script.

--- prototype/start.sh
+++ prototype/start.sh
@@ -7,4 +7,6 @@ TRAIN_ARGS_YAML=${BASE_DIR}/"config.yaml"
 
 ARGS="--train_args_yaml ${TRAIN_ARGS_YAML}"
 
-python main.py ${ARGS}
+CUDA_VISIBLE_DEVICES=0 python main.py ${ARGS}
+
+# torchrun --nproc_per_node=8 main.py ${ARGS}
