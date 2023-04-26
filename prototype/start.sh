5dcf68616d5e6aa85d8a1aaa017a2c56e55c719b
KnightZhang625 <knight.625@live.com>
2023-04-26 15:17:53 +0000

    :sparkles: v1.0: support multi-gpu

--- prototype/start.sh
+++ prototype/start.sh
@@ -7,6 +7,10 @@ TRAIN_ARGS_YAML=${BASE_DIR}/"config.yaml"
 
 ARGS="--train_args_yaml ${TRAIN_ARGS_YAML}"
 
-CUDA_VISIBLE_DEVICES=0 python main.py ${ARGS}
+# single gpu
+# CUDA_VISIBLE_DEVICES=0 python main.py ${ARGS}
 
-# torchrun --nproc_per_node=8 main.py ${ARGS}
+# two gpus on one node
+torchrun --nproc_per_node=2 main.py ${ARGS}
+
+# python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py ${ARGS}
