8637ee17046191cd8dc8f7f1b975f3a4786d6d78
KnightZhang625 <knight.625@live.com>
2023-05-01 11:17:05 +0000

    :wrench: v1.6

--- prototype/start.sh
+++ prototype/start.sh
@@ -8,9 +8,9 @@ TRAIN_ARGS_YAML=${BASE_DIR}/"config.yaml"
 ARGS="--train_args_yaml ${TRAIN_ARGS_YAML}"
 
 # single gpu
-CUDA_VISIBLE_DEVICES=0 python main.py ${ARGS}
+# CUDA_VISIBLE_DEVICES=0 python main.py ${ARGS}
 
 # two gpus on one node
-# torchrun --nproc_per_node=2 main.py ${ARGS}
+torchrun --nproc_per_node=2 main.py ${ARGS}
 
 # python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py ${ARGS}
