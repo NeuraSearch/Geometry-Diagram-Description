31106a536582cad0b517f3f65a7dae28f75cd495
Jiaxin Zhang <gvb20207@archie-t.hpc.strath.ac.uk>
2023-05-08 16:55:35 +0100

    :wrench: v2.5

--- prototype/start.sh
+++ prototype/start.sh
@@ -8,7 +8,7 @@ TRAIN_ARGS_YAML=${BASE_DIR}/"config.yaml"
 ARGS="--train_args_yaml ${TRAIN_ARGS_YAML}"
 
 # single gpu
-CUDA_VISIBLE_DEVICES=0 python main.py ${ARGS}
+# CUDA_VISIBLE_DEVICES=0 python main.py ${ARGS}
 
 # two gpus on one node
 # torchrun --nproc_per_node=2 main.py ${ARGS}
@@ -16,4 +16,4 @@ CUDA_VISIBLE_DEVICES=0 python main.py ${ARGS}
 # python -m torch.distributed.launch --nproc_per_node=2 --use_env main.py ${ARGS}
 
 # train program generator
-# CUDA_VISIBLE_DEVICES=0 python main_gp.py ${ARGS}
+CUDA_VISIBLE_DEVICES=0 python main_gp.py ${ARGS}
